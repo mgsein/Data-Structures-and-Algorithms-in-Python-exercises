e3ab7700e0ad5ccfcd5d540edadf7b4c32f39b0c
Chow Wain Sein @ Mg Aung Hein <tommysnow.01@gmail.com>
2025-12-12 19:56:38 +0700

    added linked list files

---
+++ 2. Linked Lists/2. Linked Lists/1. Building Linked Lists/build_linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LL:
+    def __init__(self):
+        self.head = None 
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        
+        current = self.head 
+        while current.next:
+            current = current.next
+        current.next = new_node 
+
+    def print(self):
+        output = []
+        current = self.head
+        while current:
+            output.append(str(current.data))
+            current = current.next 
+        print('->'.join(output))
+
+ll = LL()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+
+ll.print()
---
+++ 2. Linked Lists/2. Linked Lists/4. Inserting Nodes/removing_duplicate_nodes.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LL:
+    def __init__(self):
+        self.head = None 
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+
+        if self.head.data > data:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next and current.next.data < data:
+            current = current.next
+        new_node.next = current.next
+        current.next = new_node
+
+    def remove_duplicates(self):
+        pass
+
+    def print(self):
+        output = []
+        current = self.head
+        while current:
+            output.append(str(current.data))
+            current = current.next 
+        print('->'.join(output))
+
+elements = [2,2,3,3,3,4,5]
+
+ll = LL()
+for e in elements:
+    ll.insert(e)
+ll.print()
+
+ll.remove_duplicates()
+ll.print()
