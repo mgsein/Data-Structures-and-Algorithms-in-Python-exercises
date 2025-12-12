87fee34003970c869f0c2d550332266e1c28177e
Chow Wain Sein @ Mg Aung Hein <tommysnow.01@gmail.com>
2025-12-12 20:00:58 +0700

    advanced algorithms

---
+++ 5. Advanced Algorithmic Thinking with Python/2. Dynamic Programming/zero_one_knapsack_start.py
@@ -0,0 +1,12 @@
+from tabulate import tabulate
+
+
+def knapsack(max_capacity, weights, values):
+    pass
+
+
+values = [10, 5, 20, 35]
+weights = [1, 2, 3, 5]
+max_capacity = 6
+
+print(knapsack(max_capacity, weights, values))
